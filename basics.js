69d66887964b16a0760e6f39af01d583d7123f13
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-07-18 17:52:12 +0100

    basics.js

---
+++ basics.js
@@ -0,0 +1,24 @@
+// Primitive: number string boolean
+// More complex type: array, object
+// function types, parameters
+// Primitive 
+var age;
+age = 23;
+var hobbies;
+hobbies = "Football";
+var isInstructor;
+isInstructor = false;
+var users;
+users = ["ram", "goat"];
+var people;
+people = [{
+        name: "sadiq",
+        age: 56
+    }];
+var students;
+students = [
+    { name: "wisdom", course: "Bch 201", marNo: 14 },
+    { name: "grace", course: "Bch 201", marNo: 60 },
+];
+var course = "Typescript course";
+course = 1234;
